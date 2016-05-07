63823750e92e79e4a0ce7560adc6c0b3265db0e0
rodrigoMrtz <rodrigomartinez789389@gmail.com>
2016-05-07 00:31:49 -0400

    Adicionado consulta e página veículos

--- sitecar/shopcar/views.py
+++ sitecar/shopcar/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from .models import Veiculo
+from django.views.generic.list import ListView
 
 def index(response):
     veiculos = Veiculo.objects.all()
     return render(response, 'shopcar/index.html', {'veiculos': veiculos})
+
+class ListaVeiculo(ListView):
+    template_name = 'shopcar/veiculos/veiculo_list.html'
+    model = Veiculo
+    paginate_by = 35
+
+    def get_queryset(self):
+        v = Veiculo.objects.all()
+        q = self.request.GET.get('pesquisar_por')
+
+        # Buscar por veiculo
+        if q is not None:
+            v = v.filter(modelo__icontains=q)
+        return v
+
+    def index(response):
+        veiculos = Veiculo.objects.all()
+        return render(response, 'shopcar/index.html', {'veiculos': veiculos})
--- sitecar/sitecar/urls.py
+++ sitecar/sitecar/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from shopcar.views import *
 from django.contrib import admin
+from django.views.generic import ListView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', 'shopcar.views.index', name='home'),
     url(r'^index$', 'shopcar.views.index', name='home'),
+    url(r'^veiculos/$', ListaVeiculo.as_view(), name='ListaVeiculo'),
 ]
